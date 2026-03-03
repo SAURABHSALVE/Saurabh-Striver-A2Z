d509c2dda6d4d7dec7ab4d5ad97d16a29d71df29
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-03-03 16:17:29 +0530

    add

---
+++ arrays/findsum.py
@@ -0,0 +1,13 @@
+def array_sum(arr):
+    n = len(arr)
+    if n == 0:
+        return 0 
+    if n == 1:
+        return arr[0]
+    sum = 0
+    for i in range(n):
+        sum += arr[i]
+    return sum 
+
+print(array_sum([1,2,3,4]))
+    
