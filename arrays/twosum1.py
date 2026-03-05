a88b5a46737aa720882d9914dd77caf86e36fb35
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-03-05 13:07:20 +0530

    i am the best

--- arrays/twosum1.py
+++ arrays/twosum1.py
@@ -28,3 +28,14 @@ def sum3(arr,target):
         hashset.add(arr[i])
     return False
 print(sum3([1,2,3,4],5))
+
+## usin the hashing to print the indices of the two numbers in an array whose sum is equal to the target
+def sum4(arr,target):
+    hashset = {}
+    n = len(arr)
+    for i in range(n):
+        if target - arr[i] in hashset:
+            return [hashset[target - arr[i]],i]
+        hashset[arr[i]] = i
+    return False
+print(sum4([1,2,3,4],5))
