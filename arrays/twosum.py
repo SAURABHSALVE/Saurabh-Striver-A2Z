9ae78eb63f15aab13c2eec6880efbc59c13688c4
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-03-05 13:12:28 +0530

    i am the best

--- arrays/twosum.py
+++ arrays/twosum.py
@@ -1,9 +1,9 @@
 ## optimal solution using hashmap
-def twoSum(nums, target):
-    hashset = {}
+def twoSum(nums,target):
+    hashmap = {}
     n = len(nums)
     for i in range(n):
-        if target - nums[i] in hashset:
-            return [hashset[target - nums[i]],i]
-        hashset[nums[i]] = i
-    return False
+        if target - nums[i] in hashmap:
+            return [hashmap[target - nums[i]],i]
+        hashmap[nums[i]] = i
+print(twoSum([1,2,3,4],5))
