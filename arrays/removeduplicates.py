222d44e2e82b9d2f07974b5d3651537a19b584a0
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-03-05 14:30:47 +0530

    i am the best

--- arrays/removeduplicates.py
+++ arrays/removeduplicates.py
@@ -1,10 +1,11 @@
 def remove(arr):
-    n = len(arr)
     i = 0
+    n = len
     for j in range(1,n):
         if arr[i] != arr[j]:
             i+=1
         arr[i] = arr[j]
     return i+1
 print(remove([1,1,2,2,3,4,5]))
+
         
