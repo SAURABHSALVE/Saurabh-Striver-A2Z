dc4c865601d41d0e45c0720a09851e30538a2a0f
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-03-05 13:17:14 +0530

    i am the best

---
+++ arrays/removeduplicates.py
@@ -0,0 +1,10 @@
+def remove(arr):
+    n = len(arr)
+    i = 0
+    for j in range(1,n):
+        if arr[i] != arr[j]:
+            i+=1
+        arr[i] = arr[j]
+    return i+1
+print(remove([1,1,2,2,3,4,5]))
+        
