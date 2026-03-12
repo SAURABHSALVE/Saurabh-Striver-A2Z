90c44fd85cb21612882e30b7238201e4685dd852
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-03-12 12:38:26 +0530

    added the upadtion for practice

--- arrays/checkarraysorted.py
+++ arrays/checkarraysorted.py
@@ -1,10 +1,13 @@
 def checkarray(arr):
+
     n = len(arr)
-    
+
     for i in range(1, n):
+
         if arr[i] < arr[i-1]:
             return False
-            
+
     return True
 
-print(checkarray([1,2,3,4]))
+
+print(checkarray([1,2,3,4,5]))
--- arrays/countevenodd.py
+++ arrays/countevenodd.py
@@ -1,18 +1,14 @@
-# def evenodd(arr):
-#     n = len(arr)
-#     even = 0 
-#     odd = 0 
-#     for i in range(n):
-#         if arr[i]%2== 0:
-#             even +=1
-#         else:
-#             odd +=1 
-        
-#     return [even,odd]
-# print(evenodd([1,2,4,5]))
-
-
-
+def evenodd(arr):
+    n = len(arr)
+    even = 0
+    odd = 0
+    for i in range(n):
+        if arr[i]%2== 0:
+            even+=1
+        else:
+            odd +=1
+    return [even,odd]
+print(evenodd([1,2,3,4,5]))
 
 
 
