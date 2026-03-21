b0aa012d75992fa1d644e49391e754e18d0d8440
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-03-21 12:57:31 +0530

    added the kadanes algorithm

--- arrays/maximum-subarray-sum.py
+++ arrays/maximum-subarray-sum.py
@@ -1,16 +1,17 @@
-def maximumSubArraySum(arr):
+def maximum_subarray_sum(arr):
     maxi = float('-inf')
     sum = 0
-    n = len(arr)
-    for i in range(n):
+    for i in range(len(arr)):
         if sum <0:
             sum = 0
-            sum+=arr[i]
+            sum += arr[i]
         else:
-            sum+=arr[i]
+            sum += arr[i]
         maxi = max(maxi,sum)
-    return maxi
-
+    return maxi 
 # Example usage:
 arr = [-2,1,-3,4,-1,2,1,-5,4]
-print(maximumSubArraySum(arr))  # Output: 6 (subarray [4,-1,2,1])
+print(maximum_subarray_sum(arr))  # Output: 6 (subarray [4,-1,2,1])
+
+## command to run the code
+# python maximum-subarray-sum.py
