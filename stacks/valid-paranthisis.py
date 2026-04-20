78d1fff52d0f8eb7af0d36f028c5cf53ec75434c
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-04-20 15:16:27 +0530

    added the stack code

---
+++ stacks/valid-paranthisis.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        closeToopen = {")":"(", "]":"[","}":"{"}
+
+        for c in s:
+            if c in closeToopen:
+                if stack and stack[-1] == closeToopen[c]:
+                    stack.pop()
+                else:
+                    return False
+            
+            else:
+                stack.append(c)
+            
+        return True if not stack else False
