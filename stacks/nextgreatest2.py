4ca3a0456559ac6541b890813eb0406c3443c4e4
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-04-25 23:04:44 +0530

    added the code

--- stacks/nextgreatest2.py
+++ stacks/nextgreatest2.py
@@ -1,23 +1,21 @@
 class Solution(object):
     def nextGreaterElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
         n = len(nums)
-        res = [-1] * n
-        stack = []
         
-        # Iterate backwards through a virtual doubled array
-        for i in range(2 * n - 1, -1, -1):
-            current = nums[i % n]
-            
-            # Pop elements that are smaller or equal to the current element
-            while stack and stack[-1] <= current:
+        nge = [-1]*n 
+        stack = []
+        for i in range(2  x x cc c          v         v v vv   *n-1,-1,-1):
+            current = nums[i%n]
+            while stack and stack[-1]<=current:
                 stack.pop()
-            
-            # Only update the result during the first pass (actual indices)
             if i < n:
                 if stack:
-                    res[i] = stack[-1]
+                    nge[i] = stack[-1]
             
-            # Add current element to stack for the next comparison
             stack.append(current)
-            
-        return res
+        return nge 
+### this is the code
