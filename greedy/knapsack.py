02fb0281a0fcb88756e7ae7cec869cd0373d037f
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-04-28 23:38:26 +0530

    added the cdoe

--- greedy/knapsack.py
+++ greedy/knapsack.py
@@ -1,37 +1,28 @@
-class Item:
-    def __init__(self, value, weight):
-        self.value = value
-        self.weight = weight
+def fractionalknapsack(weights,values,capacity):
+    items = []
+    for i in range(len(values)):
+        items.append({
+            'v': values[i],
+            'w': weights[i],
+            'ratio': values[i]/weights[i]
+        })
     
-
-def fractionalKnapsack(W, arr, n):
-    arr.sort(key=lambda x: (x.value / x.weight), reverse=True)
-    curWeight = 0 
-    finalvalue = 0.0 
-    for i in range(n):
-        if curWeight + arr[i].weight <= W:
-            curWeight += arr[i].weight
-            finalvalue += arr[i].value 
+    items.sort(key= lambda x:x['ratio'],reverse = True)
+    total_value = 0
+    for item in items:
+        if capacity == 0:
+            break
+        if item['w']<= capacity:
+            capacity-=item['w']
+            total_value+=item['v']
         else:
-            remain = W - curWeight
-            finalvalue += (arr[i].value / arr[i].weight) * remain
-            break 
-    return finalvalue
+            total_value+=item['ratio']*capacity    
+            capacity = 0 
+    return total_value
 
-def main():
-    # Input data
-    n = 3
-    weight = 50  # Capacity of knapsack
-    arr = [Item(100, 20), Item(60, 10), Item(120, 30)]
-
-    # Calculate the maximum value we can get with the fractional knapsack
-    ans = fractionalKnapsack(weight, arr, n)
-
-    # Output the result
-    print(f"The maximum value is: {ans:.2f}")
-
-# Call the main function
-if __name__ == "__main__":
-    main()
-    
-    
+weights = [10,20,30]
+values = [60,100,120]
+capacity = 50
+print(fractionalknapsack(weights,values,capacity))  ### answer is 240.0
+### command to run: python greedy/knapsack.py
+##maintaing the streak
