68e16fbcb1fdd7a80aa69863a01c061143c6d21b
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-04-29 12:00:55 +0530

    added the code

---
+++ greedy/paranthesis.py
@@ -0,0 +1,22 @@
+def paranethesis(s):
+    low = 0 
+    high = 0
+    for i in s:
+        if s[i] == '(':
+            low +=1 
+            high += 1 
+        elif s[i] == ')':
+            low -= 1 
+            high -= 1
+        elif s[i] == '*':
+            low -= 1 
+            high += 1
+        if high < 0:
+            return False
+        if low < 0:
+            low = 0 
+    return low == 0
+print(paranethesis("(*)")) ### answer is True
+print(paranethesis("(*))")) ### answer is True
+          
+    
