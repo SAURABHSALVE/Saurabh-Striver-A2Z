9d8c7f2c062c0fed843cf3a0eadc1e68a0e1c778
Saurabh <saurabhsalve9999@gmail.com>
2026-06-06 13:40:28 +0530

    added the code

--- Dynamic-Programming/climbing-stairs.py
+++ Dynamic-Programming/climbing-stairs.py
@@ -1,11 +1,24 @@
+# def climbing_stairs(n):
+#     if n <= 2:
+#         return n 
+#     dp = [0]*(n+1)
+#     dp[1] = 1 
+#     dp[2] = 2 
+#     for i in range(3,n+1):
+#         dp[i] = dp[i-1]+dp[i-2]
+#     return dp[n]
+# print(climbing_stairs(5))  # Output: 8
+# print(climbing_stairs(10)) # Output: 89
+
+
+
 def climbing_stairs(n):
-    if n <= 2:
-        return n 
-    dp = [0]*(n+1)
-    dp[1] = 1 
-    dp[2] = 2 
-    for i in range(3,n+1):
-        dp[i] = dp[i-1]+dp[i-2]
-    return dp[n]
+    one , two  = 1,1
+    for i in range(2,n+1):
+        one, two = two, one + two
+    return two
 print(climbing_stairs(5))  # Output: 8
 print(climbing_stairs(10)) # Output: 89
+
+
+## python command to run this is python Dynamic-Programming/climbing-stairs.py
--- Dynamic-Programming/coin-change.py
+++ Dynamic-Programming/coin-change.py
@@ -12,5 +12,11 @@ class Solution(object):
                 if i - coin>= 0:
                     dp[i] = min(dp[i], 1 + dp[i - coin])
         return dp[amount] if dp[amount] != float('inf') else -1
+# Test cases
+solution = Solution()
+print(solution.coinChange([1, 2, 5], 11))  # Output: 3
+print(solution.coinChange([2], 3))         # Output: -1
+print(solution.coinChange([1], 0))         # Output: 0
+
 
         
